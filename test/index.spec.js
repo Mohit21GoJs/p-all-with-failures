f49de087a637b9a29435c9971e40875be307e5dc
Mohit Yadav <mohit.yadav@xebia.com>
2019-03-09 11:33:35 +0400

    update unit tests to increase coverage and use cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -12,11 +12,20 @@ describe('promiseAllWithFailures', () => {
   });
 
   it('should call failure cb and return the failure cb return value', async () => {
+    const failureCb = jest.fn(() => 21);
     await expect(
       index([
-        { func: Promise.reject(1), failureCb: () => 21 },
+        { func: Promise.reject(1), failureCb },
         { func: Promise.resolve(2) },
       ]),
     ).resolves.toEqual([21, 2]);
+    expect(failureCb).toBeCalledWith(1);
+  });
+
+  it('should return error from rejects', async () => {
+    const error = new Error('reject promise');
+    await expect(
+      index([{ func: Promise.reject(error) }, { func: Promise.resolve(2) }]),
+    ).resolves.toEqual([error, 2]);
   });
 });
